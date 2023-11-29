8699b0d72296df6c2d392c13413916e6697c8a3a
Nathaniel Mon Père <47852617+natevmp@users.noreply.github.com>
2023-11-29 13:30:23 +0000

    Delete HPC directory

--- HPC/interactiveSesh.sh
+++
@@ -1,7 +0,0 @@
-qlogin -l h_vmem=16G
-
-wget https://julialang-s3.julialang.org/bin/linux/x64/1.8/julia-1.8.5-linux-x86_64.tar.gz
-tar zxvf julia-1.8.5-linux-x86_64.tar.gz
-
-export PATH="$PATH:~/julia-1.8.5/bin"
-export JULIA_NUM_THREADS=10
